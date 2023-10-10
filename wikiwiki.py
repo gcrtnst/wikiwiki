7b66e7f26598c349a53b58058ae45ae3f4f77a82
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2023-10-10 21:55:43 +0900

    Add files

---
+++ wikiwiki.py
@@ -0,0 +1,38 @@
+import argparse
+import bs4
+import requests
+import urllib.parse
+
+
+def main():
+    argp = argparse.ArgumentParser()
+    subp = argp.add_subparsers(dest="cmd", required=True)
+
+    getp = subp.add_parser("get")
+    getp.add_argument("wiki")
+    getp.add_argument("page")
+
+    args = argp.parse_args()
+    if args.cmd == "get":
+        get(args.wiki, args.page)
+    else:
+        raise RuntimeError
+
+
+def get(wiki, page):
+    url = (
+        "https://wikiwiki.jp/"
+        + urllib.parse.quote(wiki, safe="", encoding="utf-8", errors="strict")
+        + "/?cmd=edit&page="
+        + urllib.parse.quote_plus(page, safe="", encoding="utf-8", errors="strict")
+    )
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    soup = bs4.BeautifulSoup(resp.text, "html.parser")
+    text = soup.select_one("#original-data-temp").attrs["value"]
+    print(text, end="")
+
+
+if __name__ == "__main__":
+    main()
