dea55ab1b9f0e42bec3daa0819bd3557134fe9a4
gcrtnst <52910071+gcrtnst@users.noreply.github.com>
2025-10-07 23:19:10 +0900

    Replace get command to gethtml command

--- wikiwiki.py
+++ wikiwiki.py
@@ -1,5 +1,6 @@
 import argparse
 import bs4
+import copy
 import requests
 import urllib.parse
 
@@ -8,18 +9,19 @@ def main() -> None:
     argp = argparse.ArgumentParser()
     subp = argp.add_subparsers(dest="cmd", required=True)
 
-    getp = subp.add_parser("get")
+    getp = subp.add_parser("gethtml")
     getp.add_argument("wiki")
     getp.add_argument("page")
+    getp.add_argument("--pretty", action="store_true")
 
     args = argp.parse_args()
-    if args.cmd == "get":
-        get(args.wiki, args.page)
+    if args.cmd == "gethtml":
+        gethtml(args.wiki, args.page, pretty=args.pretty)
     else:
         raise RuntimeError
 
 
-def get(wiki: str, page: str) -> None:
+def gethtml(wiki: str, page: str, *, pretty: bool = False) -> None:
     sess = requests.Session()
     try:
         del sess.headers["User-Agent"]
@@ -29,15 +31,21 @@ def get(wiki: str, page: str) -> None:
     url = (
         "https://wikiwiki.jp/"
         + urllib.parse.quote(wiki, safe="", encoding="utf-8", errors="strict")
-        + "/?cmd=edit&page="
-        + urllib.parse.quote_plus(page, safe="", encoding="utf-8", errors="strict")
+        + "/"
+        + urllib.parse.quote_plus(page, safe="/", encoding="utf-8", errors="strict")
     )
     resp = sess.get(url)
     resp.raise_for_status()
 
     soup = bs4.BeautifulSoup(resp.text, "html.parser")
-    tag = soup.select_one("#original-data-temp")
-    text = tag.attrs["value"] if tag is not None else ""
+    elem = soup.select_one("#content")
+    text = ""
+    if elem is not None:
+        elem = copy.copy(elem)
+        for chld in elem.select("div.caption-flybox") + elem.select("script"):
+            chld.decompose()
+
+        text = elem.prettify() if pretty else str(elem)
     print(text, end="")
 
 
